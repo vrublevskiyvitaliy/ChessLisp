5b78ebbbf8eb181c490df28ce1a846be7e4d89ee
vrublevskiyvitaliy <vrublevskiy@lun.ua>
2017-05-12 22:54:49 +0300

    refactor board

--- src/board.lisp
+++ src/board.lisp
@@ -5,30 +5,30 @@
 
 (defclass piece ()
   ((color :type color :accessor color :initarg :color)
-   (kind :type piece-kind :accessor kind :initarg :kind :documentation "Тип фигуры."))
-  (:documentation "Базовый класс для всех фигур."))
+   (kind :type piece-kind :accessor kind :initarg :kind :documentation "Тип фігури."))
+  (:documentation "Базовий клас для всіх фігур."))
 
 
 (defclass board ()
   ((pieces
     :initform (make-array 65)
     :accessor pieces
-    :documentation "Массив объектов типа piece. Если поле свободно, то элемент равен nil. Нулевой элемент массива не используется.")
+    :documentation "Масив обєктів типу piece. Якщо поле вільне, то елемент = nil. Нульвий елемент масиву не використовується.")
    (turn
     :type color :initform :white
     :accessor turn
-    :documentation "Who's moving next")
+    :documentation "Хто ходить наступний")
    (castlings
     :accessor castlings
-    :documentation "Which castlings are allowed. List of squares c1,g1,c8,g8")
+    :documentation "Які рокіровки дозволені. Список c1,g1,c8,g8")
    (enpassant
     :accessor enpassant
-    :documentation "Square where enpassant is possible"))
-  (:documentation "Описывает положение фигур на доске."))
+    :documentation "Клітинки де можливе взяття на проході"))
+  (:documentation "Опис положення фігур на дошці."))
 
 
 (defun copy-board (board)
-  "Создает копию объекта board."
+  "Створює копію обєкту board."
   (let ((copy (shallow-copy-object board)))
     (setf (pieces copy) (copy-seq (pieces board)))
     copy))
@@ -37,7 +37,7 @@
 
 
 (defgeneric whos-at (board sq)
-  (:documentation "Возвращает для поля SQ фигуру и её цвет (два значения) или nil.")
+  (:documentation "Повертає для поля SQ фігуру та її колір (два значення) або nil.")
   (:method ((board board) sq)
     (let ((w (aref (pieces board) sq)))
       (if w (values w (color w)) nil ))))
@@ -47,7 +47,7 @@
   (null (whos-at board square)))
 
 (defgeneric load-from-fen-string (board string)
-  (:documentation "Инициализирует позицию из строки в нотации FEN."))
+  (:documentation "Ініціалізує позицію з рядка в нотації FEN."))
 
 
 (defun piece-to-name (piece)
@@ -57,7 +57,7 @@
 
 
 (defgeneric print-board (board &key stream)
-  (:documentation "Выводит текущее положение в стандартной нотации.")
+  (:documentation "Виводить теперішнє положення в стандартній нотації.")
   (:method ((board board) &key (stream t))
     (format stream "~A"
             (with-output-to-string (s)
@@ -75,28 +75,28 @@
 
 
 (defgeneric print-board-fen (board &key stream)
-  (:documentation "Выводит текущее положение в формате FEN.")
+  (:documentation "Виводить теперішнє положення в форматі FEN.")
   (:method ((board board) &key (stream t) )
     (print-board board :stream stream)))
 
 
 (defgeneric find-pieces (board piece color)
-  (:documentation "Находит поля на которых стоят фигуры типа PIECE цвета COLOR. Если PIECE равно nil, то находит все фигуры указазнного цвета. Возвращает список полей, на которых сотоят фигуры.")
+  (:documentation "Знаходить поля на яких стоять фігури типу PIECE кольору COLOR. Якщо PIECE = nil, то знаходить всі фігури даного кольору. Вертає список полів, на яких стоять фігури.")
   (:method ((board board) piece-kind color)
     (loop :for sq :from 1 :to 64
        :when (and (not (null (aref (pieces board) sq)))
-		  (or (not piece-kind)
-		      (eql piece-kind (kind (aref (pieces board) sq))))
-		  (eql color (color (aref (pieces board) sq))))
+                  (or (not piece-kind)
+                      (eql piece-kind (kind (aref (pieces board) sq))))
+            (eql color (color (aref (pieces board) sq))))
        :collecting sq)))
 
 
 (defgeneric value-of (object &key)
-  (:documentation "Вычисляет оценку заданного объекта."))
+  (:documentation "Обраховує оцінку заданого обєкта."))
 
 
 (defmethod value-of (p &key)
-  "Метод, который обрабатывает типы, а не классы."
+  "Метод, обробляє типи, а не класи."
   (cond
     ((typep p 'piece-kind)
      (let ((pv '(:pawn 1 :knight 3 :bishop 3 :rook 5 :queen 9 :king 100)))
@@ -109,7 +109,7 @@
     val))
 
 (defun piece-value (piece &key (color :white))
-  "Оценка стоимости фигуры с точки зрения COLOR"
+  "Оцінка вартості фігури з точки зору COLOR"
   (if piece
       (* (value-of piece)
          (if (eq (color piece) color) +1 -1))
@@ -120,11 +120,11 @@
        (eq (color piece) color)))
 
 ;;
-;; Функции для расстановки фигур
+;; Розстановка фігур
 ;;
 
 (defgeneric clear (board)
-  (:documentation "Убирает с доски все фигуры.")
+  (:documentation "Очистити все з дошки")
   (:method ((board board))
     (progn
       (setf (slot-value board 'enpassant) nil)
@@ -135,9 +135,9 @@
 
 
 (defgeneric setup-piece (board sq piece &key)
-  (:documentation "Ставит на поле SQ фигуру PIECE.")
+  (:documentation "Ставить на поле SQ фігуру PIECE.")
   (:method ((board board) sq piece &key (replace t))
-    "Если поле SQ занаято, то фигура на этом поле заменяется новой."
+    "Якщо поле зайняте SQ, то фігура заміняється новою."
     (declare (ignorable replace))
     (setf (aref (slot-value board 'pieces) sq) piece)))
 
@@ -149,16 +149,16 @@
 
 
 (defmethod load-from-fen-string ((board board) fen-str)
-  ;; Нотация FEN - строка, состоящая из:
-  ;; - описание положения фигур (от 8-ой горизонтали к 1-ой, от a к h), заглавные
-  ;;   буквы обозначают белые фигуры, строчные - черные. Разделитель строк - '/'.
-  ;;   Цифры в строке обозначают количество пустых клеток;
-  ;; - очередности хода (b или w);
-  ;; - допустимых рокировках (KQkq);
-  ;; - поле взятия на проходе в стандартной нотации, или '-';
-  ;; - число полуходов с момента последнего хода пешки или взятия фигуры;
-  ;; - число ходов, которые сделали черные (минимальное значение - 1).
-  ;; Пример строки: rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
+  ;; Нотація FEN - рядок, що складається з:
+  ;; - опис положення фігур (від 8-ой горизонталі до 1-ой, від a до h), ВЕЛИКІ
+  ;;   букви позначають білі фігури, малі - чорні. Роздільник рядків - '/'.
+  ;;   Цифри в рядку позначають кількість пустих клітин;
+  ;; - хто ходить (b чи w);
+  ;; - допустимі рокіровки (KQkq);
+  ;; - поле взяття на проході в стандартній нотації чи '-';
+  ;; - число півходів з моменту останнього ходу пішака або взяття фігури;
+  ;; - число ходів, які зробили чорні (мінімум - 1).
+  ;; Приклад : rnbqkbnr/pp1ppppp/8/2p5/4P3/5N2/PPPP1PPP/RNBQKB1R b KQkq - 1 2
   (flet ((piece-by-name (char)
 	   (cdr (assoc (char-upcase char)
 		       '((#\K . :king) (#\Q . :queen) (#\R . :rook)
@@ -170,7 +170,7 @@
     (let ((tokens (cl-utilities:split-sequence #\Space fen-str))
 	  tok (sq 1) empty-count char)
       (clear board)
-      (setf tok (pop tokens)) ; положение фигур на доске
+      (setf tok (pop tokens)) ; положення фігур на дошці
       (loop :for row :in (reverse (cl-utilities:split-sequence #\/ tok)) :do
 	 (dotimes (i (length row))
 	   (setf char (elt row i))
@@ -180,28 +180,28 @@
 	     (setup-piece board sq
 			  (make-instance 'piece :kind (piece-by-name char) :color (piece-color char))))
 	   (incf sq empty-count)))
-      (setf tok (pop tokens)) ; чей ход
+      (setf tok (pop tokens)) ; чий хід
       (setf (turn board) (if (char= (elt tok 0) #\w) :white :black))
-      (setf tok (pop tokens)) ; рокировки
+      (setf tok (pop tokens)) ; рокіровка
       (setf (castlings board)
 	    (mapcar #'(lambda (c) (cdr (assoc c '((#\K . #@g1@) (#\Q . #@c1@)
 						  (#\k . #@g8@) (#\q . #@c8@)))))
 		    (coerce tok 'list)))
-      (setf tok (pop tokens)) ; взятие на проходе
+      (setf tok (pop tokens)) ; взяття на проході
       (setf (enpassant board) (if (char= (elt tok 0) #\-) nil (string-to-square tok)))
-      (setf tok (pop tokens)) ; число полуходов
-      (setf tok (pop tokens)) ; число ходов
+      (setf tok (pop tokens)) ; число півходів
+      (setf tok (pop tokens)) ; число ходів
       t)))
 
 
 (defgeneric new-game (board)
-  (:documentation "Начальная позиция.")
+  (:documentation "Початкова позиція.")
   (:method ((board board))
     (load-from-fen-string board "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")))
 
 
 
-;;; Передвижение фигуры
+;;; Рух фігур
 
 (defun move-piece (board from to)
   (setf (aref (pieces board) to) (aref (pieces board) from))
@@ -209,15 +209,15 @@
 
 
 (defgeneric make-move (board move &key)
-  (:documentation "Делает ход на доске.")
+  (:documentation "Зробити хід на дошці.")
   (:method ((the-board board) move &key (clone-board nil))
-    "Если clone-board не равно nil, то BOARD не изменяется и ход возвращается копия BOARD со сделанным на ней ходом."
+    "Якщо clone-board != nil, то BOARD нне змінюється і хід повертає копію BOARD зі зробленим на ній ходом."
     (let* ((board (if clone-board (copy-board the-board) the-board))
 	   (from (move-from move))
 	   (to (move-to move))
 	   (piece (whos-at board from))
 	   (color (color piece)))
-      ;; обработка рокировок; перставить ладью и запрет рокировок
+      ;; обробка рокіровок; переставити туру і заборонити рокіровки
       (let* ((e1e8 (if (eql color :white) #@e1@ #@e8@))
 	     (c1c8 (- e1e8 2))
 	     (g1g8 (+ e1e8 2))
@@ -228,7 +228,7 @@
 	   (setf (castlings board)
 		 (remove-if #'(lambda (x) (member x '(c1c8 g1g8))) (castlings board)))
 	   (when (castling-move-p piece color move)
-	     (move-piece board rook-sq-from rook-sq-to))) ; переносим ладью
+	     (move-piece board rook-sq-from rook-sq-to))) ; переносимо туру
 	  ((= from (- e1e8 4)) ; a1/8
 	   (setf (castlings board) (remove c1c8 (castlings board))))
 	  ((= from (+ 3 e1e8)) ; h1/8
@@ -244,9 +244,9 @@
   (new-game the-board))
 
 
-;;; макросы
+;;; макрос
 (defmacro do-pieces ((board bind-vars &key (color nil)) &body body)
-  "Выплняет BODY для всех значений SQ, которые соответствуе полям, на которых стоят фигуры цвета COLOR."
+  "Виконує BODY для всіх значень SQ, які відповідають полям на яких стоять фігури кольору COLOR."
   (let (piece-bind (sq-bind (gensym)))
     (when (and (listp bind-vars) (= (length bind-vars) 2))
       (setf piece-bind (first bind-vars))
@@ -259,12 +259,11 @@
 		(or (null ,color) (eql ,color (color (aref (pieces ,board) x-sq)))))
        (let ((,piece-bind (aref (pieces ,board) x-sq))
 	     (,sq-bind x-sq))
-	 (declare (ignorable ,piece-bind) (ignorable ,sq-bind)) ;--- FIXME: generate nesessery bindings only
+	 (declare (ignorable ,piece-bind) (ignorable ,sq-bind))
 	 ,@body)))))
 
 (defmacro with-move ((board from to) &body body)
-  "Evaluates BODY in an implicit PROGN with move FROM-TO made on the
-BOARD. Undo changes made by the move after BODY evaluation."
+  "Обраховує BODY в PROGN з ходом FROM-TO made на BOARD. Відміняє зміни викликані ходом після обрахунку."
   (let ((piece-at-to (gensym)))
   `(let ((,piece-at-to (whos-at ,board ,to)))
      (move-piece ,board ,from ,to)
